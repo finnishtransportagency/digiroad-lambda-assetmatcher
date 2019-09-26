90c68683707288ab9b2d0c92561f742ba827648d
FilPinto <filipe.pinto@cgi.com>
2019-09-26 14:23:20 +0100

    DROTH-2115 (US 2122) Generate DataSet Id on Lambda Function -Creates datasetid and insert into the table with GeoJson -Returns the datasetid to the user

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import psycopg2
 import base64
-
+import uuid
 
 def lambda_handler(event, context):
     try:
@@ -10,28 +10,30 @@ def lambda_handler(event, context):
                                       host = os.environ['RDS_HOST'],
                                       port = os.environ['RDS_PORT'],
                                       database = os.environ['RDS_DATABASE'])
-        connection.set_isolation_level(0)                              
+        connection.set_isolation_level(0)
         cursor = connection.cursor()
         data = event['body']
-        insert_query="INSERT into datasets (json_data) values ('%s')" %(data)
-        cursor.execute(insert_query)
-        script=open("matching_script.sql","r").read()
+        datasetId = str(uuid.uuid4())
+        insert_query = "INSERT INTO datasets (dataset_id, json_data) VALUES (%s, %s)"
+        insert_data = (datasetId, data)
+        cursor.execute(insert_query, insert_data)
+        script = open("matching_script.sql", "r").read()
         cursor.execute(script)
-        fetch_query="SELECT edges FROM temp_points limit 1;"
+        fetch_query = "SELECT edges FROM temp_points limit 1;"
         cursor.execute(fetch_query)
-        result=cursor.fetchall()
-        truncate_query="truncate temp_points;"
+        result = cursor.fetchall()
+        truncate_query = "truncate temp_points;"
         cursor.execute(truncate_query)
-    except (Exception, psycopg2.Error) as error :
+    except (Exception, psycopg2.Error) as error:
         print ("Error while connecting to PostgreSQL", error)
     finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-    
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
     return {
         'statusCode': 200,
-        'body': json.dumps(result[0])
-        }
+        'body': json.dumps(datasetId)
+    }
