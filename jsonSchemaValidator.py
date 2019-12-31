991acdb761ea4cb2998a444c5c2ba73d22cd1c5f
FilPinto <filipe.pinto@cgi.com>
2019-12-31 11:07:15 +0200

    DROTH-2236 Improve JSON-schema validation in Lambda-assetmatcher -Changed functional class to expect a string

--- jsonSchemaValidator.py
+++ jsonSchemaValidator.py
@@ -80,7 +80,11 @@ linear_properties_schema = {
             "type": "string"
         },
         "functionalClass": {
-            "enum": [1, 2, 3, 4, 5, 6, 7, 8]
+            "type": "string",
+            "minLength": 1
+            # Probably will be changed to receive a code value or we will invent rules to convert their string values
+            # to match our functional classes
+            # "enum": [1, 2, 3, 4, 5, 6, 7, 8]
         },
         "speedLimit": {
             "enum": [20, 30, 40, 50, 60, 70, 80, 90, 100, 120]
