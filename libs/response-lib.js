8ba310f612a7eb17e4a5c3632f0d301bd03d6f24
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2020-01-31 05:29:43 +0200

    HTTP status codes for Accepted and Not Found

--- libs/response-lib.js
+++ libs/response-lib.js
@@ -2,16 +2,24 @@ export function success(body) {
   return buildResponse(200, body);
 }
 
-export function failure(body) {
-  return buildResponse(500, body);
+export function accepted(body) {
+  return buildResponse(202, body);
+}
+
+export function badrequest(body) {
+  return buildResponse(400, body);
 }
 
 export function forbidden(body) {
   return buildResponse(403, body);
 }
 
-export function badrequest(body) {
-  return buildResponse(400, body);
+export function notfound(body) {
+  return buildResponse(404, body);
+}
+
+export function failure(body) {
+  return buildResponse(500, body);
 }
 
 function buildResponse(statusCode, body) {
